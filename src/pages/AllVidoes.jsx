afa94e3f85d456a4af612687bd5bdc86ec828ad3
SHDPIXEL <shdpixel@gmail.com>
2025-01-24 22:19:18 +0530

    Update AllVidoes.jsx

--- src/pages/AllVidoes.jsx
+++ src/pages/AllVidoes.jsx
@@ -29,7 +29,7 @@ const AllVideo = () => {
           All Uploaded Videos
         </h2>
         <br/>
-        <p>All Videos Will Published Soon.</p>
+        <p class="text-red">All Videos Will Published Soon.</p>
 
         <section className="space-y-8 mt-8">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
