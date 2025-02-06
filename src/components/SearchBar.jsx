0e8a91fc8de32477d45b7840f25a1bcb67491875
Nandan Pathak <nandanpathak30@gmail.com>
2025-02-06 13:08:41 +0530

    add: searchbar responsiveness

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react';
-import { Search, X } from 'lucide-react';
-import API from '../api';
+import { useState } from "react";
+import { Search, X } from "lucide-react";
+import API from "../api";
 
-const SearchBar = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+const SearchBar = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState("");
   const [isFocused, setIsFocused] = useState(false);
 
   const handleClear = () => {
-    setSearchQuery('');
+    setSearchQuery("");
+    onSearch("");
   };
 
   const handleSearch = async () => {
     if (!searchQuery.trim()) return;
-    console.log(searchQuery)
     try {
       const response = await API.post("/auth/user/searchVideos", {
-        query: searchQuery
-      })
+        query: searchQuery,
+      });
+      onSearch(response.data.data);
     } catch (error) {
-      console.error("Search Error", error)
+      console.error("Search Error", error);
+      onSearch([]);
     }
-
-  }
+  };
 
   return (
-    <div className={`relative flex items-center w-96 h-10 rounded-md border transition-all duration-200 ${isFocused ? 'border-gray-400 shadow-sm' : 'border-gray-200'
-      }`}>
+    <div
+      className={`relative flex items-center w-full max-w-sm md:w-96 h-10 rounded-md border transition-all duration-200 ${
+        isFocused ? "border-gray-400 shadow-sm" : "border-gray-200"
+      }`}
+    >
       <div className="flex items-center justify-center bg-gray-200 rounded-l-md w-10 h-full pointer-events-none">
-        <Search className="w-4 h-4 text-gray-400" />
+        <Search className="w-5 h-5 text-gray-400 md:w-4 md:h-4" />
       </div>
       <input
         type="text"
@@ -36,15 +40,18 @@ const SearchBar = () => {
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
         placeholder="Search Videos..."
-        className={`w-full h-full px-2 bg-gray-200 text-sm text-gray-900 placeholder:text-gray-400 focus:outline-none`}
+        className="w-full h-full px-3 bg-gray-200 text-sm text-gray-900 placeholder:text-gray-400 focus:outline-none"
         onKeyDown={(e) => e.key === "Enter" && handleSearch()}
       />
       <button
         onClick={handleClear}
-        className={`flex items-center justify-center bg-gray-200 rounded-r-md w-10 h-full hover:text-gray-600`}
+        className="flex items-center justify-center bg-gray-200 rounded-r-md w-10 h-full hover:text-gray-600"
       >
-        <X className={`w-4 h-4 text-gray-400 hover:text-gray-600 transition-colors ${searchQuery ? "opacity-100" : "opacity-0"
-          }`} />
+        <X
+          className={`w-5 h-5 text-gray-400 md:w-4 md:h-4 hover:text-gray-600 transition-colors ${
+            searchQuery ? "opacity-100" : "opacity-0"
+          }`}
+        />
       </button>
     </div>
   );
